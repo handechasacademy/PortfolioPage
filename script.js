ea764b19bfa740b9aa65f7aba623f05704b014ba
Hande Bengü <hande.bengu@chasacademy.se>
2026-02-06 16:47:40 +0100

    General restyling + Add additional sections

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
+
+// Tech stack animation
+const techStack = document.getElementById('techStack');
+const techStackContainer = document.querySelector('.techStackContainer');
+
+const items = Array.from(techStack.children);
+const stackWidth = techStack.scrollWidth;
+const containerWidth = techStackContainer.offsetWidth;
+
+let duplicates = Math.ceil(containerWidth / stackWidth) + 3;
+
+for (let i = 0; i < duplicates; i++) {
+  items.forEach(item => {
+    techStack.appendChild(item.cloneNode(true));
+  });
+}
+
+let stackPosition = 0;
+const stackSpeed = 0.5;
+
+function slideTechStack() {
+    stackPosition -= stackSpeed;
+
+    if (Math.abs(stackPosition) >= techStack.scrollWidth / 2) {
+        stackPosition = 0;
+    }
+
+    techStack.style.transform = `translateX(${stackPosition}px)`;
+    requestAnimationFrame(slideTechStack);
+}
+
+slideTechStack();
+
+
+// Walker animation
 const walker = document.querySelector('.timelineWalker');
 const wrapper = document.querySelector('.timelineWrapper');
-const speed = 0.4; 
+const walkerSpeed = 0.4;
 
 let topPosition = 0;
 
 function animateWalker() {
     const wrapperHeight = wrapper.clientHeight;
 
-    topPosition += speed;
+    topPosition += walkerSpeed;
 
     if (topPosition > wrapperHeight) {
         topPosition = -walker.offsetHeight;
@@ -19,3 +54,4 @@ function animateWalker() {
 }
 
 window.addEventListener('load', animateWalker);
+
